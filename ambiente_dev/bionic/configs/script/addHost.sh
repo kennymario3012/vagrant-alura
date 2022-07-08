5f15c9a45ef0ac818576343a44172ae3155da67d
Kenny Mário <kennymarioantonio@gmail.com>
2022-07-08 10:48:52 +0100

    feat: refacturing phpweb and mysqldb machine to execut remote ansible playbook

--- ambiente_dev/bionic/configs/script/addHost.sh
+++ ambiente_dev/bionic/configs/script/addHost.sh
@@ -3,12 +3,12 @@
 
 cd ~/Documents/STUDY/ALURA/DEVOPS/Vangrant/ambiente_dev/bionic/
 
-IP=$(vagrant ssh mysqldb -c "hostname -I | cut -d' ' -f2" )
-
-echo "[mysqlserver]
+IP=$(vagrant ssh $1 -c "hostname -I | cut -d' ' -f2" )
+echo $IP e $1
+echo "[$1]
 $IP
-[mysqlserver:vars]
+[$1:vars]
 ansible_user=vagrant
 ansible_ssh_private_key_file=/home/mdadmin/Documents/STUDY/ALURA/DEVOPS/Vangrant/ambiente_dev/bionic/configs/keys/id_bionic
 ansible_python_interpreter=/usr/bin/python3
-ansible_ssh_common_args='-o StrictHostKeyChecking=no'" > ~/Documents/STUDY/ALURA/DEVOPS/Vangrant/ambiente_dev/bionic/configs/ansible/hosts
+ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> ~/Documents/STUDY/ALURA/DEVOPS/ansible_alura/wordpressSite/hosts
